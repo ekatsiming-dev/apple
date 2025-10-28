f739aca0ed6c5277abdde5b57651d22e3cef0c83
ekatsiming-dev <ekatsiming@gmail.com>
2025-10-28 18:59:51 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+// config.js
+// 集中管理 API 網址和 App ID
+export const API_URL = 'https://script.google.com/macros/s/AKfycbwqvLcasnoWBj4FO_GYi5hxpzkkTF5dix4K0HZ9N4MoBzQc2K1S4Co8Lnz5vcjB7WhX/exec';
+export const appId = 'default-app-id';
+
+// 集中管理 Tailwind 色票
+export const tailwindColors = {
+    customAppleYellow: {
+        50: '#FCFAF5',  // 更淡的奶油黃 (背景)
+        500: '#B34B62', // 玫瑰紅 (邊框/焦點)
+        600: '#9D324E', // 酒紅色 (主要按鈕/文字)
+        700: '#6D2833'  // 深栗色 (按鈕 Hover)
+    }
+};

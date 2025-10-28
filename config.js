ba6ad9c98cc75f72e5cd0d2465b1334afc8bd5ef
ekatsiming-dev <ekatsiming@gmail.com>
2025-10-28 19:00:18 +0800

    Update config.js
    https://script.google.com/macros/s/AKfycbz6PfmdrVdHEWOyBUskzCIR3ubwJviCd_kvRi2-XbWnzVxcGqYhHSvRb_Z88hI1Sghq/exec

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 // 集中管理 API 網址和 App ID
-export const API_URL = 'https://script.google.com/macros/s/AKfycbwqvLcasnoWBj4FO_GYi5hxpzkkTF5dix4K0HZ9N4MoBzQc2K1S4Co8Lnz5vcjB7WhX/exec';
+export const API_URL = 'https://script.google.com/macros/s/AKfycbz6PfmdrVdHEWOyBUskzCIR3ubwJviCd_kvRi2-XbWnzVxcGqYhHSvRb_Z88hI1Sghq/exec';
 export const appId = 'default-app-id';
 
 // 集中管理 Tailwind 色票
